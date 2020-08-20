e44e79db47832fab1d46e12ff9625874450c96b3
singhanshu12345 <singhanshu12345@gmail.com>
2020-08-20 16:14:45 +0100

    Initial code for assignment class 6

---
+++ assignmentClass6/QueueImplement.java
@@ -0,0 +1,94 @@
+package assignmentClass6;
+
+import java.util.Scanner;
+
+interface queue{
+	public void insert(int n);
+	public void delete();
+	public void display();
+}
+public class QueueImplement implements queue {
+	int size;
+	int front;
+	int rear;
+	int len;
+	int queue[];
+	
+	QueueImplement(int n){
+		size=n;
+		front=-1;
+		rear=-1;
+		len=0;
+		queue=new int[n];
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch;
+		System.out.println("Enter the length of the queue: ");
+		Scanner in = new Scanner(System.in);
+		int size=in.nextInt();
+		QueueImplement qim1 = new QueueImplement(size);
+		
+		do {
+			System.out.println("1. Insert in the queue: ");
+			System.out.println("2. Delete from the queue: ");
+			System.out.println("3. Display the queue: ");
+			System.out.println("\n Enter the choice: ");
+			int choice=in.nextInt();
+			switch(choice) {
+			case 1:
+				try {
+					System.out.println("Enter the value: ");
+					qim1.insert(in.nextInt());
+				}
+				catch(Exception e){
+					System.out.println(e.getMessage());
+				}
+				break;
+			case 2:
+				try {
+					qim1.delete();
+				}
+				catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case 3:
+				try {
+					qim1.display();
+				}
+				catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			default:
+				System.out.println("Wrong Choice");
+				break;
+			}
+			System.out.println("Do u want to continue");
+			ch=in.next().charAt(0);
+		}
+		
+		while(ch=='y'||ch=='Y');
+		
+	}
+	public void insert(int num) {
+		if(rear+1>=size) {
+			throw new IndexOutOfBoundsException("Overflow");			
+		}
+		else if(rear+1<size) {
+			queue[++rear]=num;
+		}
+	}
+	public void delete() {
+		System.out.println("This is inside delete method");
+	}
+	public void display() {
+		for(int i=1;i<=queue.length;i++) {
+			System.out.println("Element "+i+":"+queue[i-1]);
+		}
+	}
+	
+
+}
